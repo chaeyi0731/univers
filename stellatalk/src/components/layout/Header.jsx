839c8fbeaa9329a04d21684aec057dd2e67c308f
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 13:03:33 +0900

    :sparkles: Feat: header 작업

---
+++ stellatalk/src/components/layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../assets/image/stellachatlogo.png'; // 로고 이미지 경로로 변경하세요.
+import './header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <div className="fake"></div>
+        <img className="bgcimg" src={logo} alt="StellaChat Logo" />
+
+        <div className="Info">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
