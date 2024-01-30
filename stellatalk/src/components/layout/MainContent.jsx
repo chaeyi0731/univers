1ab4c7b9b0a86257fc293646b8e1a41451515874
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 15:18:43 +0900

    :sparkles: Feat: main content 시작

---
+++ stellatalk/src/components/layout/MainContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main className="main-content">
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </main>
+  );
+}
+
+export default MainContent;
