3e62ca4caae28fd1571cda249a514ddbc84b6b61
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 17:18:38 +0900

    다른 페이지들 연결

--- stellatalk/server/router/router.jsx
+++ stellatalk/server/router/router.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import ChatPage from '../../src/pages/chatPage';
+import PostPage from '../../src/pages/postPage';
+import UniversePage from '../../src/pages/universePage';
+import MainContent from '../../src/components/layout/MainContent';
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="/" element={<MainContent />}></Route>
       <Route path="/chat" element={<ChatPage />}></Route>
+      <Route path="/post" element={<PostPage />}></Route>
+      <Route path="/universe" element={<UniversePage />}></Route>
     </Routes>
   );
 };
