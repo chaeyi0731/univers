fc582e45a5623406deee5d4a3dd559b711f0074e
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 14:35:54 +0900

    :coffin: Remove: 안쓰이는 crl 코드 제거

--- stellatalk/src/App.jsx
+++ stellatalk/src/App.jsx
@@ -3,17 +3,7 @@ import './App.css';
 import Header from './components/layout/Header';
 
 function App() {
-  return (
-    <Header>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </Header>
-  );
+  return <Header></Header>;
 }
 
 export default App;
