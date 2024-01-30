1c4173d60146aa97de4480fb94c13e033bdc6ad8
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 17:18:23 +0900

    :hammer: Modify: 덜 작성 된 부분 작성하기

--- stellatalk/server/app.js
+++ stellatalk/server/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/chat', (req, res)=>
-);
+app.get('/chat', (req, res) => {
+  res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
