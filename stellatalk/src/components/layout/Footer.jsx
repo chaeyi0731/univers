9e1fae7ad62cfc80d4b874880605182f4c611fd5
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 14:35:37 +0900

    :coffin: Remove: 코드 개선을 위해 안쓰는 코드 삭제

--- stellatalk/src/components/layout/Footer.jsx
+++ stellatalk/src/components/layout/Footer.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundImage: `url('path-to-your-background-image')` }}>
-      {' '}
-      {/* 배경 이미지 경로로 변경하세요. */}
-      {/* 소셜 미디어 아이콘들 - 필요에 따라 수정하세요. */}
-      <a href="http://facebook.com">Facebook</a>
-      <a href="http://twitter.com">Twitter</a>
-      <a href="http://instagram.com">Instagram</a>
-      {/* 저작권 정보 - 필요에 따라 수정하세요. */}
-      <p>&copy; {new Date().getFullYear()} StellaChat</p>
+    <footer>
+      <div className="footer">
+        {/* a를 link to 로 바꾸어야함 */}
+        <a href="http://facebook.com">Facebook</a>
+        <a href="http://twitter.com">Twitter</a>
+        <a href="http://instagram.com">Instagram</a>
+      </div>
     </footer>
   );
 };
