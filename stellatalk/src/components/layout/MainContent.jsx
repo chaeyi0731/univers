d5f4e2ce5928c830c62256e5b6ea3ce55217e933
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 16:02:41 +0900

    :hammer: Modify: 이미지를 따로 삽입 및 p태그 추가

--- stellatalk/src/components/layout/MainContent.jsx
+++ stellatalk/src/components/layout/MainContent.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import './layout.css';
 
 function MainContent() {
   return (
     <main className="main-content">
       <div className="content">
-        <div className="chat" ></div>
-        <div className="post"></div>
-        <div className="universe"></div>
+        <div className="chat">
+          <div></div>
+          <p className="mainp">CHAT</p>
+        </div>
+        <div className="post">
+          <div></div>
+          <p className="mainp">POST</p>
+        </div>
+        <div className="universe">
+          <div></div>
+          <p className="mainnp">UNIVERSE</p>
+        </div>
       </div>
     </main>
   );
