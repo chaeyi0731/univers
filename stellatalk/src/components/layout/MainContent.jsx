058f2417cf3dfd0961652f41b3816c3d3c5517b3
ChaeYi <chaeyi990731@gmail.com>
2024-01-30 17:19:55 +0900

    :hammer: Modify: linkto 가 안먹혀서 대체체로 온클릭으로 사용

--- stellatalk/src/components/layout/MainContent.jsx
+++ stellatalk/src/components/layout/MainContent.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-
 import './layout.css';
 
 function MainContent() {
+  const handleClick = (path) => {
+    window.location.href = path;
+  };
+
   return (
     <main className="main-content">
       <div className="content">
-        <div className="chat">
+        <div className="chat" onClick={() => handleClick('/chat')}>
           <div></div>
           <p className="mainp">CHAT</p>
         </div>
-        <div className="post">
+        <div className="post" onClick={() => handleClick('/post')}>
           <div></div>
           <p className="mainp">POST</p>
         </div>
-        <div className="universe">
+        <div className="universe" onClick={() => handleClick('/universe')}>
           <div></div>
           <p className="mainnp">UNIVERSE</p>
         </div>
