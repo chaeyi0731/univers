6d6e2341e7bdd57f05ab5abde9e5f39b0f6d9634
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 11:06:43 +0900

    :poop: Bad: 번역 실패 그러나 서버는 실행

--- stellatalk/src/server/app.js
+++ stellatalk/src/server/app.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001; // 포트 변경 가능
+// const axios = require('axios');
 
 const cors = require('cors');
 app.use(cors());
+
+
+//? 번역 api 문제로 인해 주석 처리
+// const subscriptionKey = 'ae664ba92c324d5ba399188a33bf7bbe';
+// const endpoint = 'https://api.cognitive.microsofttranslator.com';
+// const location = 'stella-talk';
+
+// app.get('/translate', async (req, res) => {
+//   const { text, lang } = req.query; // 클라이언트로부터 받은 텍스트와 목표 언어
+
+//   axios({
+//     baseURL: endpoint,
+//     url: '/translate',
+//     method: 'post',
+//     headers: {
+//       'Ocp-Apim-Subscription-Key': subscriptionKey,
+//       'Ocp-Apim-Subscription-Region': location,
+//       'Content-Type': 'application/json',
+//     },
+//     params: {
+//       'api-version': '3.0',
+//       to: [lang],
+//     },
+//     data: [
+//       {
+//         text: text,
+//       },
+//     ],
+//   })
+//     .then((response) => {
+//       res.json(response.data[0].translations[0].text);
+//     })
+//     .catch((error) => {
+//       console.error('Error during translation:', error);
+//       res.status(500).send('Translation error');
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
