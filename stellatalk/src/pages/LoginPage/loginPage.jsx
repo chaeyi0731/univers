449c16bfbb24f31613c000098f35748cbd60ace9
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 12:18:09 +0900

    :art: Design: 디자인을 위한 클래스지정

--- stellatalk/src/pages/LoginPage/loginPage.jsx
+++ stellatalk/src/pages/LoginPage/loginPage.jsx
@@ -1,7 +1,23 @@
 const LoginPage = () => {
   return (
     <div className="main-content">
-      <div className="login-widgets"></div>
+      <div className="login-widgets">
+        <div className="login-top">
+          <h1>LOG IN</h1>
+          <input className="login-input" type="text"></input>
+          <input className="login-input" type="password"></input>
+        </div>
+        <div className="login-bottom">
+          <div className="fined">
+            <div>아이디찾기/</div>
+            <div>비밀번호찾기</div>
+          </div>
+          <div className="login-btn">
+            <button className="login-button"> 로그인하기 </button>
+            <button className="signup-button"> 회원가입하기</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
