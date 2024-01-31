bef4547e208d0f273939ec37c2391889aaf4158b
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 11:31:51 +0900

    :hammer: Modify: 라우터 설정

--- stellatalk/src/server/router/router.jsx
+++ stellatalk/src/server/router/router.jsx
@@ -3,12 +3,17 @@ import ChatPage from '../../pages/chatPage';
 import PostPage from '../../pages/postPage';
 import UniversePage from '../../pages/universePage';
 import MainContent from '../../components/layout/MainContent';
-
+import LoginPage from '../../pages/LoginPage/loginPage';
+import SignupPage from '../../pages/SignupPage/signupPage';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<MainContent />} />
+      {/* 헤더 로그인/회원가입 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="signup" element={<SignupPage />} />
+      {/* 컨텐츠 영역 */}
       <Route path="/chat" element={<ChatPage />} />
       <Route path="/post" element={<PostPage />} />
       <Route path="/universe" element={<UniversePage />} />
