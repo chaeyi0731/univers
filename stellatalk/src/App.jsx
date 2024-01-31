dda228782a4733d57325ded4c324ecded38f3094
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 15:02:16 +0900

    :hammer: Modify: 로그인 전역상태를 위한 코드 수정

--- stellatalk/src/App.jsx
+++ stellatalk/src/App.jsx
@@ -4,14 +4,19 @@ import AppRouter from './server/router/router';
 import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import { UserProvider } from './content/UserContext';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <AppRouter />
-      <Footer />
-    </Router>
+    <UserProvider>
+      {' '}
+      {/* UserProvider를 Router 바깥쪽에 추가 */}
+      <Router>
+        <Header />
+        <AppRouter />
+        <Footer />
+      </Router>
+    </UserProvider>
   );
 }
 
