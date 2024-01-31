303ec43536e8b4ce412002b2155e2f34163d5ad8
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 16:39:12 +0900

    :poop: Bad: 로그인 수정이 안된다.

--- stellatalk/src/pages/LoginPage/loginPage.jsx
+++ stellatalk/src/pages/LoginPage/loginPage.jsx
@@ -11,21 +11,16 @@ const LoginPage = () => {
   const handleLoginClick = async () => {
     try {
       const response = await login(username, password);
-      // 성공 여부를 response.data.success로 판단
-      if (response.data.success) {
-        // 로그인 성공
-        navigate('/'); // 메인 페이지로 이동
+      if (response && response.data && response.data.success) {
+        navigate('/'); // 로그인 성공 시 메인 페이지로 이동
       } else {
-        // 로그인 실패 처리
         alert('로그인 실패');
       }
     } catch (error) {
       console.error('로그인 요청 실패', error);
-      // error.response.data.message 등을 사용하여 서버에서 반환한 오류 메시지를 표시할 수 있음
-      alert(error.response.data.message || '로그인 요청 실패');
+      alert('로그인 요청 실패');
     }
   };
-
   return (
     <div className="main-content">
       <div className="login-widgets">
