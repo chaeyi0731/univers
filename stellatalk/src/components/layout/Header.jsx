cdd8df3e4de340c5627f62561605fc5e6b06bca8
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 15:02:59 +0900

    :hammer: Modify: 전역으로 로그인 확인 및 로그인시 user name 가져오기

--- stellatalk/src/components/layout/Header.jsx
+++ stellatalk/src/components/layout/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from '../../assets/image/stellachatlogo.png'; // 로고 이미지 경로로 변경하세요.
 import './layout.css';
+import React, { useContext } from 'react';
+import { UserContext } from '../../content/UserContext';
+import logo from '../../assets/image/stellachatlogo.png'; // 로고 이미지 경로로 변경하세요.
 
 const Header = () => {
+  const { user, logout } = useContext(UserContext);
   const handleClick = (path) => {
     window.location.href = path;
   };
@@ -13,8 +15,14 @@ const Header = () => {
         <img className="bgcimg" src={logo} alt="StellaChat Logo" onClick={() => handleClick('/')} />
 
         <div className="Info">
-          <button onClick={() => handleClick('/login')}>Login</button>
-          <button onClick={() => handleClick('/signup')}>Sign Up</button>
+          {user ? (
+            <>
+              <span>{user.name}님 안녕하세요</span>
+              <button onClick={logout}>로그아웃</button>
+            </>
+          ) : (
+            <button onClick={() => handleClick('/login')}>Login</button>
+          )}
         </div>
       </div>
     </header>
