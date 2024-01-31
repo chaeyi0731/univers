054ccbbd4728ab0ea15d12f2c5cbd6dc46834ceb
ChaeYi <chaeyi990731@gmail.com>
2024-01-31 12:56:46 +0900

    :art: Design: 디자인을 위한 클래스 부여

--- stellatalk/src/pages/SignupPage/signupPage.jsx
+++ stellatalk/src/pages/SignupPage/signupPage.jsx
@@ -1,7 +1,25 @@
 const SignupPage = () => {
   return (
-    <div>
-      <h1>login페이지</h1>
+    <div className="main-content">
+      <div className="signup-widgets">
+        <h1>SING UP</h1>
+        <div className="text-left">
+          <p>아이디</p>
+          <input className="user-info" type="text"></input>
+          <p>비밀번호</p>
+          <input className="user-info" type="password"></input>
+          <p>이름 </p>
+          <input className="user-info" type="text"></input>
+          <p>핸드폰 </p>
+          <input className="user-info" type="text"></input>
+          <p>우편번호</p>
+          <input className="user-info" type="text"></input>
+          <p>주소</p>
+          <div className="user-info" type="text"></div>
+
+          <button className="signup-button"> 가입하기 </button>
+        </div>
+      </div>
     </div>
   );
 };
