721538ef8a6ad0cd4c3c486879cd1797d3368993
ChaeYi <chaeyi990731@gmail.com>
2024-02-01 10:04:52 +0900

    :hammer: Modify: UserProvider 라우터 내부로 이동

--- stellatalk/src/App.jsx
+++ stellatalk/src/App.jsx
@@ -1,22 +1,20 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { UserProvider } from './content/UserContext';
 import AppRouter from './server/router/router';
-import './App.css';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-import { UserProvider } from './content/UserContext';
 
 function App() {
   return (
-    <UserProvider>
-      {' '}
-      {/* UserProvider를 Router 바깥쪽에 추가 */}
-      <Router>
+    <Router>
+      <UserProvider>
         <Header />
         <AppRouter />
         <Footer />
-      </Router>
-    </UserProvider>
+      </UserProvider>
+    </Router>
   );
 }
 
