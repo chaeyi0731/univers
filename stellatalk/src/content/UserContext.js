159606310639e226b55eb21045bc9905e36f14d5
ChaeYi <chaeyi990731@gmail.com>
2024-02-01 09:31:03 +0900

    :hammer: Modify: 상태 업데이트

--- stellatalk/src/content/UserContext.js
+++ stellatalk/src/content/UserContext.js
@@ -1,5 +1,5 @@
 // src/context/UserContext.js
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 import axios from 'axios';
 
 export const UserContext = createContext(null);
@@ -9,30 +9,20 @@ export const UserProvider = ({ children }) => {
 
   const login = async (username, password) => {
     try {
-      const response = await axios.post('/api/login', { username, password });
+      const response = await axios.post('http://localhost:5001/api/login', { username, password });
       if (response.data.success) {
-        setUser(response.data.user); // 로그인 성공 시 사용자 정보 저장
-        console.log(user);
+        setUser(response.data.user); // 사용자 정보 저장
+      } else {
+        alert('로그인 실패');
       }
-      return response;
     } catch (error) {
       console.error('로그인 요청 실패', error);
-      throw error;
     }
   };
-  useEffect(() => {
-    console.log(user); // 상태 업데이트 확인
-  }, [user]);
-
-  console.log(user);
 
   const logout = async () => {
-    try {
-      await axios.post('/api/logout');
-      setUser(null); // 사용자 상태 초기화
-    } catch (error) {
-      console.error('로그아웃 요청 실패', error);
-    }
+    await axios.post('http://localhost:5001/api/logout');
+    setUser(null); // 사용자 상태 초기화
   };
 
   return <UserContext.Provider value={{ user, login, logout }}>{children}</UserContext.Provider>;
