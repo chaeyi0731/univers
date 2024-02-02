45f2ab221b5bcb6ad93e19cdf5207e0e23d9f581
ChaeYi <chaeyi990731@gmail.com>
2024-02-02 16:29:32 +0900

    :hammer: Modify: 로그인 한 사람만 게시판 이용가능

--- stellatalk/src/pages/postPage.jsx
+++ stellatalk/src/pages/postPage.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
+import '../components/layout/layout.css';
 
 const PostPage = () => {
   const [titles, setTitles] = useState([]);
+  const { user } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 사용자가 로그인하지 않은 경우 로그인 페이지로 리다이렉션
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
 
   useEffect(() => {
     fetch('/get')
