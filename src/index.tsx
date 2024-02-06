f96b8c11c1374e9aa4ffe94de5891256d4f08e28
ChaeYi <chaeyi990731@gmail.com>
2024-02-06 18:05:50 +0900

    :hammer: Modify: ts문법대로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
