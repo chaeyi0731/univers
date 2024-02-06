3c7675e0819c9734bfeb1234122464d6d4e323d3
ChaeYi <chaeyi990731@gmail.com>
2024-02-06 11:37:02 +0900

    :bug: Fix: 두번 설정된 변수 제거

--- src/server/app.js
+++ src/server/app.js
@@ -5,12 +5,6 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const socketIo = require('socket.io')(server, {
-  cors: {
-    origin: '*', // 모든 출처 허용으로 변경 또는 필요에 따라 조정
-    methods: ['GET', 'POST'],
-  },
-});
 const cors = require('cors');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
