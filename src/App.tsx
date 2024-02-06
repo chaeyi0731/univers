96d95a9bb03024e3556fd0ba25bcef0f0c3a028f
ChaeYi <chaeyi990731@gmail.com>
2024-02-06 18:05:42 +0900

    :hammer: Modify: ts문법대로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-// App.js
+// App.tsx
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { UserProvider } from './hooks/UserContext';
-import AppRouter from './server/router/router';
+import AppRouter from './server/router/router'; // 경로 확인 필요
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
