404686f0225f58911b1d526782e9ee1bd2c897d4
ChaeYi <chaeyi990731@gmail.com>
2024-02-07 10:04:42 +0900

    :bug: Fix: user 오류 UserContext를 다 가져오는것으로 해결 완료

--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -6,11 +6,13 @@ import '../components/layout/layout.css';
 const PostPage: React.FC = () => {
   const [titles, setTitles] = useState<string[]>([]);
   const navigate = useNavigate();
-  const { user } = useContext(UserContext);
+  const userContext = useContext(UserContext); // UserContext 전체를 가져옴
+
+  // UserContext가 null이거나 user 프로퍼티가 없는 경우에 대한 예외 처리 추가
+  const user = userContext?.user;
 
   useEffect(() => {
     if (!user) {
-      // 사용자가 로그인하지 않은 경우 로그인 페이지로 리다이렉트
       navigate('/login');
     }
   }, [user, navigate]);
@@ -26,15 +28,8 @@ const PostPage: React.FC = () => {
       <div className="widgets">
         <div className="postwidgets">
           <h1>게시판</h1>
-          {user ? (
-            // 사용자가 로그인한 경우에만 게시글 목록 표시
-            titles.map((title, index) => <h2 key={index}>{title}</h2>)
-          ) : (
-            // 사용자가 로그인하지 않은 경우 안내 메시지 표시
-            <p>로그인 후에 게시글을 볼 수 있습니다.</p>
-          )}
+          {user ? titles.map((title, index) => <h2 key={index}>{title}</h2>) : <p>로그인 후에 게시글을 볼 수 있습니다.</p>}
           {user && (
-            // 사용자가 로그인한 경우에만 게시글 작성 버튼 표시
             <Link to="/create-post">
               <button>게시글 작성</button>
             </Link>
