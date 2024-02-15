5fe685f954a978c5c16c25bf83e94e76b5d81fd7
ChaeYi <chaeyi990731@gmail.com>
2024-02-15 15:58:27 +0900

    :bug: Fix: soket은 모듈로 관리

--- src/server/app.js
+++ src/server/app.js
@@ -10,18 +10,9 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const { Server } = require('socket.io');
 const { exec } = require('child_process');
+const initSocket = require('./initSoket');
 
 const upload = multer({ dest: 'uploads/' });
-
-const io = new Server(server, {
-  cors: {
-    origin: 'http://13.125.146.112', // 클라이언트 주소
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['my-custom-header'],
-    credentials: true,
-  },
-  transports: ['websocket'],
-});
 
 // CORS 설정
 app.use(cors());
@@ -91,49 +82,6 @@ app.post('/api/logout', (req, res) => {
   res.send({ success: true, message: 'Successfully logged out' });
 });
 
-//? 채팅 관련 API
-
-io.of('/chatting').on('connection', (socket) => {
-  console.log('새로운 클라이언트가 /chatting에서 연결되었습니다:', socket.id);
-
-  socket.on('chat message', (msg) => {
-    io.of('/chatting').emit('chat message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('클라이언트가 /chatting에서 연결을 끊었습니다:', socket.id);
-  });
-});
-
-io.on('connection', (socket) => {
-  console.log('새로운 클라이언트가 연결되었습니다:', socket.id);
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('클라이언트가 연결을 끊었습니다:', socket.id);
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    // 모든 클라이언트에 메시지 전송
-    io.emit('chat message', msg);
-
-    // 데이터베이스에 메시지 저장
-    const query = 'INSERT INTO chatMessage (userName, message, timestamp) VALUES (?, ?, ?)';
-    db.query(query, [msg.userName, msg.text, msg.timestamp], (err, result) => {
-      if (err) {
-        console.error('메시지 저장 중 오류 발생:', err);
-        return;
-      }
-      console.log('메시지가 데이터베이스에 저장되었습니다:', result.insertId);
-    });
-  });
-});
-
 app.get('/api/chat', (req, res) => {
   const query = 'SELECT * FROM chatMessage ORDER BY timestamp DESC LIMIT 50';
   db.query(query, (err, results) => {
@@ -145,6 +93,10 @@ app.get('/api/chat', (req, res) => {
     res.json(results);
   });
 });
+
+//? 채팅관련
+
+initSocket(server);
 
 //? 게시판 게시글 관련 API
 
