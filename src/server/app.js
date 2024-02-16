b4e670f51f98e3aeb345c1bac5f710a0d2d7903d
ChaeYi <chaeyi990731@gmail.com>
2024-02-16 10:40:46 +0900

    :bug: Fix : DB포트 지정

--- src/server/app.js
+++ src/server/app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: 'universe.cliyuoye061h.ap-northeast-2.rds.amazonaws.com',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
