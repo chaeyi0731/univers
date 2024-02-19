7ce09ab3fc2135343466a991d14aed87a8e44614
ChaeYi <chaeyi990731@gmail.com>
2024-02-19 11:34:37 +0900

    :hammer: Modify: 로그인확인
    로그인 확인하여 user_id 확인 및 이미지 첨부 필수가 아님

--- src/pages/postPage/CreatePostPage.tsx
+++ src/pages/postPage/CreatePostPage.tsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../hooks/UserContext';
 
 const CreatePostPage: React.FC = () => {
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const [image, setImage] = useState<File | null>(null);
   const navigate = useNavigate();
+  const userContext = useContext(UserContext); // userContext로 변경하여 전체 컨텍스트를 받아옴
+
+  // UserContext가 null이 아닌지 확인하고 user 객체에 접근
+  const user = userContext ? userContext.user : null;
 
   // UserContext가 null이 아닌지 확인하고 user 객체에 접근
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value);
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => e.target.files && setImage(e.target.files[0]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!image) {
-      alert('이미지를 첨부해주세요.');
+
+    if (!user) {
+      // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+      navigate('/login');
       return;
     }
+
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
-    formData.append('image', image);
+    if (image) {
+      formData.append('image', image); // 이미지가 있을 때만 추가
+    }
+    formData.append('user_id', user.user_id.toString()); // 사용자 ID를 문자열로 변환하여 추가
 
     try {
       const response = await fetch(`http://localhost:3001/create-post`, {
