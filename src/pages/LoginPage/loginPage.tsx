5e28fb4afad058133a7cad8e2c51ec4d94c28215
ChaeYi <chaeyi990731@gmail.com>
2024-02-20 19:47:36 +0900

    :recycle: Refactor: 리팩토링 해보기 !

--- src/pages/LoginPage/loginPage.tsx
+++ src/pages/LoginPage/loginPage.tsx
@@ -1,10 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../hooks/UserContext';
-
-interface LoginContextType {
-  login: (username: string, password: string) => Promise<void>;
-}
+import { LoginContextType } from '../../hooks/interface';
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
