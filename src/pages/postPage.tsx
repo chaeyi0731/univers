d3789e3cc738d742ce1f7e117fc4254523883fc6
ChaeYi <chaeyi990731@gmail.com>
2024-02-20 12:00:40 +0900

    :art: Design: 게시글 작성 버튼 위치 조절

--- src/pages/postPage.tsx
+++ src/pages/postPage.tsx
@@ -38,57 +38,25 @@ const PostPage: React.FC = () => {
     fetchPosts();
   }, [navigate, userContext]);
 
-      return;
-    }
-
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('http://43.203.209.74:3001/posts');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        // 각 post의 id를 숫자로 변환
-        const postsWithCorrectId = data.map((post: any) => ({
-          ...post,
-          post_id: Number(post.post_id),
-        }));
-        setPosts(postsWithCorrectId);
-      } catch (error) {
-        console.error('There was a problem with your fetch operation:', error);
-        setError('Failed to load posts.');
+  return (
+    <div className="main-content">
+      <div className="widgets">
+        <div className="postwidgets">
+          <h1>게시판</h1>
+          {userContext?.user && (
+            <Link to="/create-post">
+              <button>게시글 작성</button>
+            </Link>
+          )}
+          {posts.map((post, index) => (
+            <div key={index}>
+              <h2>
                 <Link to={`/post/${post.id}`}>{post.title}</Link>
               </h2>
               <p>
                 작성자: {post.username}, 작성시간: {new Date(post.timestamp).toLocaleString()}
               </p>
           ))}
-
-          {userContext?.user && (
-            <Link to="/create-post">
-              <button>게시글 작성</button>
-            </Link>
-          )}
-          <table>
-            <thead>
-              <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>작성시간</th>
-              </tr>
-            </thead>
-            <tbody>
-              {posts.map((post, index) => (
-                <tr key={post.post_id} onClick={() => handleRowClick(post.post_id)} style={{ cursor: 'pointer' }}>
-                  <td>{index + 1}</td>
-                  <td>{post.title}</td>
-                  <td>{post.name}</td>
-                  <td>{new Date(post.timestamp).toLocaleString('ko-KR', { hour: '2-digit', minute: '2-digit' })}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
