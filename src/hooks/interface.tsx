73f95a670eeb3175b9df177de9faa82f5438fb6c
ChaeYi <chaeyi990731@gmail.com>
2024-02-21 09:29:43 +0900

    :recycle: Refactor: interface 모아놓기
    한번에 내보내기 위해 맨 하단에 export type 정의

--- src/hooks/interface.tsx
+++ src/hooks/interface.tsx
@@ -1,3 +1,12 @@
-export interface LoginContextType {
+interface LoginContextType {
   login: (username: string, password: string) => Promise<void>;
 }
+
+interface InputFieldProps {
+  label: string;
+  type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export type { LoginContextType, InputFieldProps };
