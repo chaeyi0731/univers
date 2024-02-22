952da793dd1ce86076971ba7a1b11afae915b794
ChaeYi <chaeyi990731@gmail.com>
2024-02-22 20:32:45 +0900

    :recycle: Refactor: 인풋, 폼 제출 버튼 부분 모듈화진행

--- src/pages/SignupPage/signupPage.tsx
+++ src/pages/SignupPage/signupPage.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
-import { UserInfo } from '../../components/common/interfaces/interfaces';
+import { useForm } from '../../hooks/useForm'; // useForm 훅 import 경로 확인
+import InputField from '../../components/common/InputField';
+import SubmitButton from '../../components/common/SubmitButtonProps';
 
 const SignupPage: React.FC = () => {
-  const [userInfo, setUserInfo] = useState<UserInfo>({
+  // useForm 훅을 사용하여 폼 상태 관리
+  const { values, handleChange } = useForm({
     username: '',
     password: '',
     name: '',
@@ -11,24 +14,17 @@ const SignupPage: React.FC = () => {
     address: '',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUserInfo((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!userInfo.username || !userInfo.password || !userInfo.name || !userInfo.phoneNumber || !userInfo.address) {
+    // values를 사용하여 폼 제출 로직 구현
+    if (!values.username || !values.password || !values.name || !values.phoneNumber || !values.address) {
       alert('모든 필드를 채워주세요.');
       return;
     }
 
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/signup`, userInfo);
+      await axios.post(`${process.env.REACT_APP_API_URL}/signup`, values);
       alert('회원가입이 완료되었습니다.');
       window.location.href = '/';
     } catch (error: any) {
@@ -41,19 +37,12 @@ const SignupPage: React.FC = () => {
       <div className="signup-widgets">
         <h1>SIGN UP</h1>
         <form onSubmit={handleSubmit}>
-          <p>아이디</p>
-          <input className="user-info" type="text" name="username" value={userInfo.username} onChange={handleInputChange} />
-          <p>비밀번호</p>
-          <input className="user-info" type="password" name="password" value={userInfo.password} onChange={handleInputChange} />
-          <p>이름</p>
-          <input className="user-info" type="text" name="name" value={userInfo.name} onChange={handleInputChange} />
-          <p>핸드폰</p>
-          <input className="user-info" type="text" name="phoneNumber" value={userInfo.phoneNumber} onChange={handleInputChange} />
-          <p>주소</p>
-          <input className="user-info" type="text" name="address" value={userInfo.address} onChange={handleInputChange} />
-          <button type="submit" className="signup-button">
-            가입하기
-          </button>
+          <InputField label="아이디" type="text" name="username" value={values.username} onChange={handleChange} />
+          <InputField label="비밀번호" type="password" name="password" value={values.password} onChange={handleChange} />
+          <InputField label="이름" type="text" name="name" value={values.name} onChange={handleChange} />
+          <InputField label="핸드폰" type="text" name="phoneNumber" value={values.phoneNumber} onChange={handleChange} />
+          <InputField label="주소" type="text" name="address" value={values.address} onChange={handleChange} />
+          <SubmitButton label="가입하기" />
         </form>
       </div>
     </div>
