2579a3ee01a78e7fde86799dcef0b0ecc82e8fc0
ChaeYi <chaeyi990731@gmail.com>
2024-02-22 12:53:27 +0900

    :poop: Bad:실패모듈

--- src/hooks/interface.tsx
+++ src/hooks/interface.tsx
@@ -1,12 +1,21 @@
+// 로그인 확인 인터페이스
 interface LoginContextType {
   login: (username: string, password: string) => Promise<void>;
 }
 
+// 안풋관련 인터페이스
 interface InputFieldProps {
   label: string;
   type: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>; // 수정된 부분
 }
 
-export type { LoginContextType, InputFieldProps };
+//post 관련 인터페이스
+interface IPost {
+  title: string;
+  content: string;
+  image: File | null;
+}
+
+export type { LoginContextType, InputFieldProps, IPost };
