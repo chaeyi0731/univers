c4d445bbea7a55239f1a0d06fe1a41f75fe7301b
ChaeYi <chaeyi990731@gmail.com>
2024-02-23 09:20:06 +0900

    :recycle: Refactor: 게시글 상세보기 div 컴포넌트화

---
+++ src/components/common/ PostDetailComponent.tsx
@@ -0,0 +1,21 @@
+// PostDetailComponent.jsx
+import React from 'react';
+import { PostDetail } from '../../components/common/interfaces/interfaces';
+
+interface PostDetailComponentProps {
+  postDetail: PostDetail | null;
+}
+
+const PostDetailComponent: React.FC<PostDetailComponentProps> = ({ postDetail }) => {
+  if (!postDetail) return null;
+
+  return (
+    <div>
+      <h1 className="post-title">{postDetail.title}</h1>
+      <p className="post-content">{postDetail.content}</p>
+      {postDetail.image_url && <img src={postDetail.image_url} alt="Post" className="post-image" />}
+    </div>
+  );
+};
+
+export default PostDetailComponent;
