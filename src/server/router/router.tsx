de9f45b724d4686ac827df886509c7359237a16a
ChaeYi <chaeyi990731@gmail.com>
2024-03-06 18:53:22 +0900

    :hammer: Modify: 메인과 서브 라우트 경로 변경

--- src/server/router/router.tsx
+++ src/server/router/router.tsx
@@ -12,14 +12,14 @@ import PostDetailPage from '../../pages/postPage/PostDetailPage';
 const Router: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainContent />} />
+      <Route path="/main" element={<MainContent />} />
       {/* 헤더 로그인/회원가입 */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="signup" element={<SignupPage />} />
       {/* 컨텐츠 영역 */}
       <Route path="/chat" element={<ChatPage />} />
       <Route path="/post" element={<PostPage />} />
-      <Route path="/universe" element={<UniversePage />} />
+      <Route path="/" element={<UniversePage />} />
       {/* 게시글 작성 라우트 */}
       <Route path="/create-post" element={<CreatePostPage />} />
       {/* 게시글 상세페이지 , 댓글 기능 페이지 */}
