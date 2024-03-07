c44d45755aaca0bfff0fc71c5565cbab70eb40b6
ChaeYi <chaeyi990731@gmail.com>
2024-03-07 12:07:20 +0900

    :hammer: Modify: 페이지 로드시 새로고침 때문에 활성상태 제거

--- src/pages/SignupPage/signupPage.tsx
+++ src/pages/SignupPage/signupPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Validation from '../../utils/validation';
+import { validateForm } from '../../utils/validation';
 import InputField from '../../components/common/InputField';
 
 const SignupPage: React.FC = () => {
@@ -13,23 +13,31 @@ const SignupPage: React.FC = () => {
     address: '',
   });
 
-  const [validationPassed, setValidationPassed] = useState(false);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setValues((prevValues) => ({ ...prevValues, [name]: value }));
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (validationPassed) {
+    e.preventDefault(); // 폼 제출 시 페이지 새로고침 방지
+
+    // 직접 유효성 검사를 호출
+    const isValid = validateForm(values);
+
+    if (isValid) {
       try {
+        // 폼 데이터가 유효한 경우, API 요청을 수행합니다.
         await axios.post(`${process.env.REACT_APP_API_URL}/signup`, values);
         alert('회원가입이 완료되었습니다.');
+        // 성공적으로 회원가입 후, 다른 페이지로 리디렉션
         window.location.href = '/main';
-      } catch (error: any) {
-        alert(error.response?.data?.message || '회원가입 중 오류가 발생했습니다.');
+      } catch (error) {
+        // API 요청 실패 시, 오류 메시지를 표시합니다.
+        alert('회원가입 중 오류가 발생했습니다.');
       }
+    } else {
+      // 유효성 검사를 통과하지 못한 경우, 적절한 메시지를 표시합니다.
+      alert('입력한 정보를 다시 확인해주세요.');
     }
   };
 
@@ -46,11 +54,8 @@ const SignupPage: React.FC = () => {
               <InputField label="이름" type="text" name="name" value={values.name} onChange={handleChange} />
               <InputField label="핸드폰" type="text" name="phoneNumber" value={values.phoneNumber} onChange={handleChange} />
               <InputField label="주소" type="text" name="address" value={values.address} onChange={handleChange} />
-              <button type="submit" disabled={!validationPassed}>
-                가입하기
-              </button>
+              <button type="submit">가입하기</button>
             </form>
-            <Validation values={values} setValidationPassed={setValidationPassed} />
           </div>
         </div>
       </div>
