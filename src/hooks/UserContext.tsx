856e8543c6ea014ea854868abd9adc1de5d630c4
ChaeYi <chaeyi990731@gmail.com>
2024-03-11 15:32:17 +0900

    :hammer: Modify: 토큰으로 로그인 관리

--- src/hooks/UserContext.tsx
+++ src/hooks/UserContext.tsx
@@ -1,4 +1,3 @@
-// UserContext.tsx
 import React, { createContext, useState, useEffect, FC } from 'react';
 import axios from 'axios';
 import { User, UserContextType, UserProviderProps } from '../components/common/interfaces/interfaces';
@@ -16,39 +15,41 @@ export const UserProvider: FC<UserProviderProps> = ({ children }) => {
     const token = localStorage.getItem('token');
     if (token) {
       axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-      axios
-        .get<User>(`${process.env.REACT_APP_API_URL}/api/verifyToken`)
-        .then((response) => {
-          setUser(response.data); // 직접 User 타입으로 상태 업데이트
-        })
-        .catch(() => {
-          localStorage.removeItem('token');
-          setUser(null);
-        });
+      verifyToken();
     }
   }, []);
+
+  const verifyToken = async (): Promise<void> => {
+    try {
+      const response = await axios.get<User>(`${process.env.REACT_APP_API_URL}/api/verifyToken`);
+      setUser(response.data);
+    } catch (error) {
+      handleTokenError();
+    }
+  };
+
+  const handleTokenError = (): void => {
+    localStorage.removeItem('token');
+    delete axios.defaults.headers.common['Authorization'];
+    setUser(null);
+    navigate('/main');
+  };
 
   const login = async (username: string, password: string): Promise<void> => {
     try {
       const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/login`, { username, password });
-      const { token } = response.data;
-      if (response.data.success && response.data.token) {
-        localStorage.setItem('token', token); // 로컬 스토리지에 토큰 저장
+      const { token, user: userData } = response.data;
+      if (response.data.success && token) {
+        localStorage.setItem('token', token);
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-        setUser(response.data.user);
-        console.log('Login response:', response);
-
+        setUser(userData);
         navigate('/main');
       } else {
-        // 서버로부터 응답은 받았지만, 로그인에 실패했을 경우
-        alert('로그인 실패: 아이디와 비밀번호를 체크하세요.');
-        navigate('/login');
+        alert('로그인 실패: 아이디와 비밀번호를 확인하세요.');
       }
     } catch (error) {
       console.error('로그인 요청 실패', error);
-      // 네트워크 에러나 서버 에러 등으로 인한 로그인 실패
-      alert('로그인 실패: 아이디와 비밀번호를 체크하세요.');
-      navigate('/login');
+      alert('로그인 실패: 네트워크 상태를 확인하세요.');
     }
   };
 
